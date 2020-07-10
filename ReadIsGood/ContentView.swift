339ca5f63256cd32326b591760ea94b5f976a902
Bagus Triyanto <44406611+bagusisgood@users.noreply.github.com>
2020-07-11 01:18:22 +0700

    Initial Commit

---
+++ ReadIsGood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReadIsGood
+//
+//  Created by Bagus Triyanto on 11/07/20.
+//  Copyright © 2020 BagusIsGood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
